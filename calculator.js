732e000ba63ed00d90f03d3b706668cbef5ffabf
shilafamaze <shilafshilaf7@gmail.com>
2024-04-24 12:15:22 +0530

    new files added

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function addition(){
+    let number1=parseInt(document.getElementById("number1").value)
+    let number2=parseInt(document.getElementById("number2").value)
+console.log(number1+number2)
+
+}
+
+function substraction(){
+    let number1=parseInt(document.getElementById("number1").value)
+    let number2=parseInt(document.getElementById("number2").value)
+console.log(number1-number2)
+
+}  
+function multiplication(){
+    let number1=parseInt(document.getElementById("number1").value)
+    let number2=parseInt(document.getElementById("number2").value)
+console.log(number1*number2)
+
+}  
+function division(){
+    let number1=parseInt(document.getElementById("number1").value)
+    let number2=parseInt(document.getElementById("number2").value)
+console.log(number1/number2)
+
+}  
+
+function modulus(){
+    let number1=parseInt(document.getElementById("number1").value)
+    let number2=parseInt(document.getElementById("number2").value)
+console.log(number1%number2)
+
+}  
